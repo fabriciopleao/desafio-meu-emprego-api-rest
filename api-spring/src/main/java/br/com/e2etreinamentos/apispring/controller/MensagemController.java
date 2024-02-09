a30022d081e8fca974305b26f5c8c5ae364b84ff
Fabricio Leão <fabriciopereiraleao@gmail.com>
2024-02-09 12:05:58 -0300

    att

--- api-spring/src/main/java/br/com/e2etreinamentos/apispring/controller/MensagemController.java
+++ api-spring/src/main/java/br/com/e2etreinamentos/apispring/controller/MensagemController.java
@@ -1,11 +1,7 @@
 package br.com.e2etreinamentos.apispring.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,19 +22,10 @@ public class MensagemController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<String> cadastrarProduto(@RequestBody Mensagem mensagem) {
+	public ResponseEntity<String> inserirMensagem(@RequestBody Mensagem mensagem) {
 		String retorno = mensagemService.inserirMensagem(mensagem);
 		return ResponseEntity.ok(retorno);	
 	}
 	
-	@GetMapping
-	public List<Object> consultarMensagens() {
-		return mensagemService.consultarMensagens();
-	}
-	
-	@DeleteMapping
-	public void excluirMensagem(@RequestBody Mensagem mensagem) {
-		mensagemService.apagarMensagem(mensagem);
-	}
 	
 }
